46dd0e741cc9dff6bbf9b8343ecf6560810a8d08
James Earls <jearls01@qub.ac.uk>
2021-10-20 20:22:56 +0100

    Added multiply function

--- src/uk/ac/qub/eeecs/dave/Calculator.java
+++ src/uk/ac/qub/eeecs/dave/Calculator.java
@@ -4,7 +4,11 @@ public class Calculator {
 	public int add(int a, int b) {
 		return a+b+1;
 	}
-	
+
+	public int multiply(int a, int b) {
+		return a*b;
+	}
+
 	public int sub(int a, int b) {
 		return a-b;
 	} 
